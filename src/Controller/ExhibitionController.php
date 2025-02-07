65016784d00688cda425fc14db0219baf0d8bcbe
Lily <112404773+Belywen@users.noreply.github.com>
2025-02-07 20:20:29 +0100

    nom id corrigé

--- src/Controller/ExhibitionController.php
+++ src/Controller/ExhibitionController.php
@@ -9,18 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ExhibitionController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function home(): Response
-    {
-        return $this->render('home.html.twig', [
-            'controller_name' => 'ExhibitionController',
-        ]);
-    }
-
     #[Route('/exhibition/{id}', name: 'exhibition')]
     public function index(Exhibition $exhibition): Response
     {
-        return $this->render('exhibition/index.html.twig', [
+        return $this->render('/exhibition/index.html.twig', [
             'exhibition' => $exhibition,
         ]);
     }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 
-use App\Entity\Exhibition;
 use App\Repository\ExhibitionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
