b9a899e5bc50a08d4e555a205809daa14989e683
Lily <112404773+Belywen@users.noreply.github.com>
2025-02-17 21:44:31 +0100

    mep police

--- src/Controller/ExhibitionController.php
+++ src/Controller/ExhibitionController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Room;
-use App\Entity\Show;
-use App\Entity\Artist;
 use App\Entity\Exhibition;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,15 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class ExhibitionController extends AbstractController
 {
     #[Route('/exhibition/{id}', name: 'exhibition')]
-    public function index(Exhibition $exhibition, Artist $artist, Room $room, Show $shows): Response
+    public function index(Exhibition $exhibition): Response
     {
-        
 
         return $this->render('/exhibition/index.html.twig', [
-            'exhibition' => $exhibition,  
-            'artist' => $artist,
-            'room' => $room,
-            'shows' => $shows,        
+            'exhibition' => $exhibition, 
         ]);
     }
 }
