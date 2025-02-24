5c8c9095d5411b659c73c97faa87776893ad3efa
Lily <112404773+Belywen@users.noreply.github.com>
2025-02-24 08:40:33 +0100

    refactorisation form security

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\Security\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
