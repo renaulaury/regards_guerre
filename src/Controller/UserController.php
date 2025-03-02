63a085cb85a661395cd0c33e51eefbe92745c5df
Lily <112404773+Belywen@users.noreply.github.com>
2025-03-02 22:26:32 +0100

    form edit identity user ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Form\UserEditIdentityFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
+
+    /*********** Affiche l'index de l'utilisateur ************************/
+    #[Route('/profile', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+
+        ]);
+    }
+
+/*********** Affiche le profil de l'utilisateur ************************/
     #[Route('/profile/{id}', name: 'profile')]
     public function profile(): Response
     {
         return $this->render('user/profile.html.twig', [
 
+        ]);
+    }
+
+/*********** Permet l'édition du nom et prénom de l'utilisateur ************************/
+    #[Route('/profile/userEditIdentity/{id}', name: 'userEditIdentity')]
+    public function editUserName(int $id, Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+
+        $form = $this->createForm(UserEditIdentityFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile', ['id' => $id]);
+        }
+
+        return $this->render('user/userEditIdentity.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UserEditIdentityFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserEditIdentityFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('userName', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'trim' => true,
+            ])
+            
+            ->add('userFirstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+                'trim' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
