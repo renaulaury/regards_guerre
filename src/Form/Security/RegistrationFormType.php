97c0717f43248ef9b4078aef3fea096a63b1ed04
Lily <112404773+Belywen@users.noreply.github.com>
2025-03-07 13:59:28 +0100

    css register et footer ok

--- src/Form/Security/RegistrationFormType.php
+++ src/Form/Security/RegistrationFormType.php
@@ -22,39 +22,55 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('userName', TextType::class, [
-                'label' => 'Nom *',  
+                'label' => 'Nom <sup>1</sup>',  
+                'label_html' => true, // Active l'interprétation HTML
+                'attr' => [
+                        'placeholder' => 'Votre nom',
+                    ],
                 'required' => false,
                 'trim' => true, //Enleve les espaces
-            ])
-            
+            ])            
             ->add('userFirstname', TextType::class, [
-                'label' => 'Prénom *',
+                'label' => 'Prénom <sup>1</sup>',  
+                'label_html' => true, // Active l'interprétation HTML
+                'attr' => [
+                        'placeholder' => 'votre prénom',
+                    ],
                 'required' => false, 
                 'trim' => true, //Enleve les espaces
             ])
+
+
             ->add('userNickname', TextType::class, [
                 'label' => 'Pseudo',  
+                'attr' => [
+                        'placeholder' => 'Votre pseudonyme',
+                    ],
                 'trim' => true, //Enleve les espaces
             ])
             ->add('userEmail', EmailType::class, [
                 'label' => 'Email',
+                'attr' => [
+                        'placeholder' => 'Votre email',
+                    ],
                 'trim' => true, //Enleve les espaces  
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez être d\'accord avec notre politique de confidentialité.',
-                    ]),
-                ],
-            ])
+
+
             ->add('password', RepeatedType::class, [
                 'mapped' => false, //cela signifie que ce champ ne sera pas directement associé à une propriété de l'entité User
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe ne sont pas identiques.',
-                'options' => ['attr' => ['class' => '']],
+                'options' => [
+                    'label' => false,
+                    'attr' => ['class' => '']
+                ],
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe',
+                'first_options'  => ['label' => 'Mot de passe <sup>2</sup>',
+                                     'label_html' => true, 
+                                     'attr' => [
+                                            'placeholder' => 'Votre mot de passe',
+                                        ],
                 'constraints' => [ // Ajout des contraintes ici
                     new NotBlank([
                         'message' => 'Veuillez entrer un mot de passe.',
@@ -66,14 +82,30 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Regex([ // Ajout de la contrainte Regex
                         'pattern' => '/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$/', //1 maj - 1 chiffre - 1 caractère spécial/word - 12 min
-                        'message' => 'Le mot de passe doit contenir au moins une majuscule, un chiffre et un caractère spécial.',
+                        'message' => 'Le mot de passe doit contenir 12 caractères et au moins une majuscule, un chiffre et un caractère spécial.',
                     ]),
                 ],
             ],
-                'second_options' => ['label' => 'Répétez le mot de passe'],
+                'second_options' => [
+                    'label' => 'Répétez le mot de passe <sup>2</sup>',
+                    'label_html' => true,
+                    'attr' => [
+                        'placeholder' => 'Répéter votre mot de passe',
+                    ],
+                ],
             ])
-    ;
-}
+            
+            
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez être d\'accord avec notre politique de confidentialité.',
+                    ]),
+                ],
+            ])
+        ;
+    }
     //Permettrait d'ajouter des options par défaut comme une entité associée
     public function configureOptions(OptionsResolver $resolver): void
     {
