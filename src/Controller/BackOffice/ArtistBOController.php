5dcac97921a7a2439ce96af45144ea6c1f8acf21
Lily <112404773+Belywen@users.noreply.github.com>
2025-03-08 21:56:50 +0100

    function delete artist ok

--- src/Controller/BackOffice/ArtistBOController.php
+++ src/Controller/BackOffice/ArtistBOController.php
@@ -24,7 +24,20 @@ final class ArtistBOController extends AbstractController
         ]);
     }
 
-    /******************** Modifier un artiste *********************/
+/******************** Ajouter un artiste *********************/
+#[Route('/backOffice/artistAddBO/{id}', name: 'artistAddBO')]
+    public function artistAddBO(ArtistBORepository $artistRepo): Response
+    {
+        
+
+        return $this->render('backOffice/artist/artistAddBO.html.twig', [
+            // 'artists' => $artists,
+        ]);
+    }
+
+
+
+/******************** Modifier un artiste *********************/
     #[Route('/backOffice/artistEditBO/{id}', name: 'artistEditBO')]
     public function artistEditBO(Request $request, Artist $artist, EntityManagerInterface $entityManager): Response
     {
@@ -37,7 +50,7 @@ final class ArtistBOController extends AbstractController
         // Vérif du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
             // Persister les modifications dans la base de données
-$entityManager->persist($artist);
+            $entityManager->persist($artist);
             $entityManager->flush();
 
             // Rediriger vers la liste des artistes
@@ -47,9 +60,40 @@ $entityManager->persist($artist);
         // Rendre le template avec le formulaire
         return $this->render('backOffice/artist/artistEditBO.html.twig', [
             'form' => $form->createView(),
-        'artist' => $artist,
+            'artist' => $artist,
         ]);
     }
 
+    /******************** Suppression un artiste *********************/
+    //Demande de suppression
+    #[Route('/backOffice/artistDeleteBO/{id}', name: 'artistDeleteBO')]
+    public function artistDeleteBO(Artist $artist): Response
+    {       
+        return $this->render('backOffice/artist/artistDeleteBO.html.twig', [            
+            'artist' => $artist,
+        ]);
+    }
+
+    //Confirmation de suppression
+    #[Route('/backOffice/artistConfirmDeleteBO/{id}', name: 'artistConfirmDeleteBO')]
+    public function artistConfirmDeleteBO(Artist $artist, EntityManagerInterface $entityManager): Response
+    {
+        // Vérifier si l'artiste est lié à une exposition
+        if (!$artist->getShows()->isEmpty()) {
+            // Ajouter un message flash pour avertir l'utilisateur
+            $this->addFlash('error', 'Impossible de supprimer cet artiste car il est affilié à une ou plusieurs expositions.');
     
+            // Rediriger l'utilisateur vers la liste des artistes ou une autre page
+            return $this->redirectToRoute('artistListBO');
+        }
+    
+        // Si l'artiste n'est pas lié à une exposition, le supprimer
+        $entityManager->remove($artist);
+        $entityManager->flush();
+    
+        // Ajouter un message flash pour confirmer la suppression
+        $this->addFlash('success', 'L\'artiste a été supprimé avec succès.');
+    
+        return $this->redirectToRoute('artistListBO');
+    }
 }
