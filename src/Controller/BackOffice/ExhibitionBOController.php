6d23b81d4214018673c80ccc139e45f369dc06b0
Lily <112404773+Belywen@users.noreply.github.com>
2025-03-17 16:41:41 +0100

    réagencement show et exhibition controller

--- src/Controller/BackOffice/ExhibitionBOController.php
+++ src/Controller/BackOffice/ExhibitionBOController.php
@@ -3,10 +3,8 @@
 namespace App\Controller\BackOffice;
 
 use App\Entity\Show;
-use App\Entity\Artist;
 use App\Entity\Exhibition;
 use App\Service\FileUploader;
-use App\Form\BackOffice\ShowAddInfosBO;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Filesystem\Filesystem;
@@ -117,122 +115,6 @@ final class ExhibitionBOController extends AbstractController
         ]);
     }
 
-    /************************* Afficher détail de l'exposition *******************/
-
-    //Détail de l'expo
-    #[Route('/backOffice/exhibitShowBO/{id}', name: 'exhibitShowBO')]
-    public function exhibitShowBO(Exhibition $exhibition, ExhibitionShareRepository $exhibitionShareRepo, EntityManagerInterface $entityManager): Response
-    {
-        //Récup tous les artistes associés à l'expo et ceux qui ne le sont pas
-        $artists = $exhibitionShareRepo->findAll();
-        $unPlanned = $exhibitionShareRepo->findUnplannedArtists($exhibition->getId());
-
-        $forms = []; //Tabl pour les forms
-        $shows = []; //Tableau pour infos artist non planifiés du show
-
-        //On boucle uniquement sur les artistes non planifiés
-        foreach ($unPlanned as $unPlannedArtist) {
-            $show = new Show(); //Création d'un nouveau show
-            $show->setExhibition($exhibition); //Injection des infos de l'expo
-            $show->setArtist($unPlannedArtist); //et de l'artiste
-
-            //Création du form pour les attributs de show
-            $form = $this->createForm(ShowAddInfosBO::class, $show);
-
-            //Stocke la vue dans le tableau grâce à l'id
-            $forms[$unPlannedArtist->getId()] = $form->createView();
-
-            // Récup artist associé au show
-             $show = $entityManager->getRepository(Show::class)->findOneBy([
-                'exhibition' => $exhibition,
-                'artist' => $unPlannedArtist,
-            ]);
-            
-            $shows[$unPlannedArtist->getId()] = $show;
-        }
-
-        return $this->render('/backOffice/exhibition/exhibitShowBO.html.twig', [
-            'exhibition' => $exhibition,
-            'artists' => $artists,
-            'unPlanned' => $unPlanned,
-            'forms' => $forms,
-            'shows' => $shows,
-        ]);
-    }
-
-    //Ajout d'un show à l'expo suite à soumission du form
-    #[Route('/backOffice/{idExhibit}/addArtistToExhibitBO/{idArtist}', name: 'addArtistToExhibitBO')]
-    public function addArtistToExhibitBO(int $idExhibit, int $idArtist, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        // Récupère l'exposition et l'artiste à partir de l'ID
-        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
-        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
-        
-        // Vérifie si l'exposition et l'artiste existent.
-        if ($exhibition && $artist) {
-            
-            $show = new Show(); //Création d'un nouveau show
-            $show->setExhibition($exhibition); //Injection des infos de lexpo
-            $show->setArtist($artist); //et de l'artiste
-
-            //Création du form pour les attributs de show
-            $form = $this->createForm(ShowAddInfosBO::class, $show);
-            $form->handleRequest($request); // Traite la requête HTTP pour remplir le form
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                // Persister et enregistre les modifications dans la base de données
-                $entityManager->persist($show);
-                $entityManager->flush();
-                $this->addFlash('success', 'Artiste ajouté avec succès.');
-            }
-
-        }
-        return $this->redirectToRoute('exhibitShowBO', ['id' => $exhibition->getId()]);
-    }
-
-    /***************** Suppression d'un artiste à l'expo ***********************/
-    //Confirmation suppression artiste à l'expo
-    #[Route('/backOffice/{idExhibit}/confirmRemoveArtistFromExhibitBO/{idArtist}', name: 'confirmRemoveArtistFromExhibitBO')]
-    public function confirmRemoveArtistFromExhibitBO(int $idExhibit, int $idArtist, EntityManagerInterface $entityManager): Response
-    {
-        // Récupère l'exposition et l'artiste à partir de l'ID
-        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
-        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
-        
-        return $this->render('backOffice/exhibition/confirmRemoveArtistFromExhibitBO.html.twig', [
-            'exhibition' => $exhibition,
-            'artist' => $artist,
-        ]);
-    }
-
-    //Suppression de l'artiste
-    #[Route('/backOffice/{idExhibit}/removeArtistFromExhibitBO/{idArtist}', name: 'removeArtistFromExhibitBO')]
-    public function removeArtistFromExhibitBO(int $idExhibit, int $idArtist, EntityManagerInterface $entityManager): Response
-    {
-        //Récup expo et l'artite via leur id
-        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
-        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
-
-
-        //Vérifie si l'expo et l'artiste existent
-        if ($exhibition && $artist) {
-            $show = $entityManager->getRepository(Show::class)->findOneBy([
-                'exhibition' => $exhibition,
-                'artist' => $artist,
-            ]);
-
-            //Vérifie si le show existe
-            if ($show) {
-                $entityManager->remove($show); //Supprime le show
-                $entityManager->flush(); //Enregistre les modifications
-                $this->addFlash('success', 'Artiste supprimé avec succès.');
-            } 
-
-            return $this->redirectToRoute('exhibitShowBO', ['id' => $idExhibit]);
-        }
-        return $this->redirectToRoute('exhibitShowBO', ['id' => $idExhibit]);
-    }
-
     /************************** Suppression d'une expo ******************************/
     #[Route('/backOffice/{idExhibit}/deleteExhibitBO', name: 'deleteExhibitBO')]
     public function deleteExhibitBO(int $idExhibit, EntityManagerInterface $entityManager): Response
@@ -246,7 +128,7 @@ final class ExhibitionBOController extends AbstractController
         ]);
     }
 
-
+    //Confirmation de suppression de l'expo
     #[Route('/backOffice/{idExhibit}/deleteConfirmExhibitBO', name: 'deleteConfirmExhibitBO')]
     public function deleteConfirmExhibitBO(int $idExhibit, EntityManagerInterface $entityManager, Filesystem $filesystem): Response
     {
@@ -278,4 +160,5 @@ final class ExhibitionBOController extends AbstractController
 
         return $this->redirectToRoute('exhibitListBO');
     }
+    
 }
---
+++ src/Controller/BackOffice/ShowBOController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+use App\Entity\Show;
+use App\Entity\Artist;
+use App\Entity\Exhibition;
+use App\Form\BackOffice\ShowAddInfosBO;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\Share\ExhibitionShareRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class ShowBOController extends AbstractController
+{
+    #[Route('/show', name: 'show')]
+    public function index(): Response
+    {
+        return $this->render('show/index.html.twig', [
+            'controller_name' => 'ShowController',
+        ]);
+    }
+
+
+    /************************* Afficher détail de l'exposition *******************/
+
+    //Détail de l'expo
+    #[Route('/backOffice/exhibitShowBO/{id}', name: 'exhibitShowBO')]
+    public function exhibitShowBO(Exhibition $exhibition, ExhibitionShareRepository $exhibitionShareRepo, EntityManagerInterface $entityManager): Response
+    {
+        //Récup tous les artistes associés à l'expo et ceux qui ne le sont pas
+        $artists = $exhibitionShareRepo->findAll();
+        $unPlanned = $exhibitionShareRepo->findUnplannedArtists($exhibition->getId());
+
+        $forms = []; //Tabl pour les forms
+        $shows = []; //Tableau pour infos artist du show
+
+        // Récupère tous les artistes déjà associés à l'exposition
+        $plannedArtists = $exhibition->getShows();
+
+        // Pour chaque artiste déjà associé, crée ou récupère le formulaire et les infos du show
+        foreach ($plannedArtists as $plannedArtist) {
+            $show = $entityManager->getRepository(Show::class)->findOneBy([
+                'exhibition' => $exhibition,
+                'artist' => $plannedArtist->getArtist(),
+            ]);
+
+            if (!$show) {
+                $show = new Show();
+                $show->setExhibition($exhibition);
+                $show->setArtist($plannedArtist->getArtist());
+            }
+
+            $form = $this->createForm(ShowAddInfosBO::class, $show);
+            $forms[$plannedArtist->getArtist()->getId()] = $form->createView();
+            $shows[$plannedArtist->getArtist()->getId()] = $show;
+        }
+
+        //On boucle uniquement sur les artistes non planifiés
+        foreach ($unPlanned as $unPlannedArtist) {
+            $show = new Show(); //Création d'un nouveau show
+            $show->setExhibition($exhibition); //Injection des infos de l'expo
+            $show->setArtist($unPlannedArtist); //et de l'artiste
+
+            //Création du form pour les attributs de show
+            $form = $this->createForm(ShowAddInfosBO::class, $show);
+
+            //Stocke la vue dans le tableau grâce à l'id
+            $forms[$unPlannedArtist->getId()] = $form->createView();
+
+            // Récup artist associé au show
+             $show = $entityManager->getRepository(Show::class)->findOneBy([
+                'exhibition' => $exhibition,
+                'artist' => $unPlannedArtist,
+            ]);
+            
+            $shows[$unPlannedArtist->getId()] = $show;
+        }
+
+        return $this->render('/backOffice/exhibition/exhibitShowBO.html.twig', [
+            'exhibition' => $exhibition,
+            'artists' => $artists,
+            'unPlanned' => $unPlanned,
+            'forms' => $forms,
+            'shows' => $shows,
+        ]);
+    }
+
+    //Ajout d'un show à l'expo suite à soumission du form
+    #[Route('/backOffice/{idExhibit}/addArtistToExhibitBO/{idArtist}', name: 'addArtistToExhibitBO')]
+    public function addArtistToExhibitBO(int $idExhibit, int $idArtist, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Récupère l'exposition et l'artiste à partir de l'ID
+        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
+        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
+        
+        // Vérifie si l'exposition et l'artiste existent.
+        if ($exhibition && $artist) {
+            
+            // Check if a Show entity already exists for this artist and exhibition
+            $show = $entityManager->getRepository(Show::class)->findOneBy([
+                'exhibition' => $exhibition,
+                'artist' => $artist,
+            ]);
+
+            // If it doesn't exist, create a new one
+            if (!$show) {
+                $show = new Show(); //Création d'un nouveau show
+                $show->setExhibition($exhibition); //Injection des infos de lexpo
+                $show->setArtist($artist); //et de l'artiste
+            }
+
+            //Création du form pour les attributs de show
+            $form = $this->createForm(ShowAddInfosBO::class, $show);
+            $form->handleRequest($request); // Traite la requête HTTP pour remplir le form
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // Persister et enregistre les modifications dans la base de données
+                $entityManager->persist($show);
+                $entityManager->flush();
+                $this->addFlash('success', 'Artiste ajouté avec succès.');
+            }
+
+        }
+        return $this->redirectToRoute('exhibitShowBO', ['id' => $exhibition->getId()]);
+    }
+    
+
+    /***************** Suppression d'un artiste à l'expo ***********************/
+    //Confirmation suppression artiste à l'expo
+    #[Route('/backOffice/{idExhibit}/confirmRemoveArtistFromExhibitBO/{idArtist}', name: 'confirmRemoveArtistFromExhibitBO')]
+    public function confirmRemoveArtistFromExhibitBO(int $idExhibit, int $idArtist, EntityManagerInterface $entityManager): Response
+    {
+        // Récupère l'exposition et l'artiste à partir de l'ID
+        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
+        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
+        
+        return $this->render('backOffice/exhibition/confirmRemoveArtistFromExhibitBO.html.twig', [
+            'exhibition' => $exhibition,
+            'artist' => $artist,
+        ]);
+    }
+
+    //Suppression de l'artiste
+    #[Route('/backOffice/{idExhibit}/removeArtistFromExhibitBO/{idArtist}', name: 'removeArtistFromExhibitBO')]
+    public function removeArtistFromExhibitBO(int $idExhibit, int $idArtist, EntityManagerInterface $entityManager): Response
+    {
+        //Récup expo et l'artite via leur id
+        $exhibition = $entityManager->getRepository(Exhibition::class)->find($idExhibit);
+        $artist = $entityManager->getRepository(Artist::class)->find($idArtist);
+
+
+        //Vérifie si l'expo et l'artiste existent
+        if ($exhibition && $artist) {
+            $show = $entityManager->getRepository(Show::class)->findOneBy([
+                'exhibition' => $exhibition,
+                'artist' => $artist,
+            ]);
+
+            //Vérifie si le show existe
+            if ($show) {
+                $entityManager->remove($show); //Supprime le show
+                $entityManager->flush(); //Enregistre les modifications
+                $this->addFlash('success', 'Artiste supprimé avec succès.');
+            } 
+
+            return $this->redirectToRoute('exhibitShowBO', ['id' => $idExhibit]);
+        }
+        return $this->redirectToRoute('exhibitShowBO', ['id' => $idExhibit]);
+    }
+    
+}
