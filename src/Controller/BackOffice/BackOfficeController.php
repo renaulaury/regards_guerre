3aed17d4330beafc75987ba0b6965a71e1a1bdbc
Lily <112404773+Belywen@users.noreply.github.com>
2025-04-20 14:54:06 +0200

    deplacement logique d affichage de msg flash

--- src/Controller/BackOffice/BackOfficeController.php
+++ src/Controller/BackOffice/BackOfficeController.php
@@ -3,15 +3,52 @@
 namespace App\Controller\BackOffice;
 
 
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\Share\ExhibitionShareRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BackOfficeController extends AbstractController
 {
     #[Route('/backOffice', name: 'backOffice')]
-    public function index(): Response
+    public function index(
+        ExhibitionShareRepository $exhibitionShareRepo, 
+        Security $security): Response
     {
+        $lastExhibitSoonSoldOut = null; //Stock d'alerte
+        $lastExhibitSoldOut = null; //Stock épuisé
+        
+        $exhibitions = $exhibitionShareRepo->findAllNextExhibition();
+        
+        foreach ($exhibitions as $exhibition) {
+            // Vérifie si le stock restant est inférieur ou égal au niveau d'alerte
+            if ($exhibition->getTicketsRemaining() <= $exhibition->getStockAlert()) {
+                $lastExhibitSoonSoldOut = $exhibition; // Stocke le nom de l'expo                
+            }   
+             // Vérifie si le stock restant est égal à zéro
+            if ($exhibition->getTicketsRemaining() <= 0) {
+                $lastExhibitSoldOut = $exhibition; 
+            }        
+        }
+    
+        
+        // Ajoute un message flash si une alerte de stock ou un stock épuisé est détecté
+        if ($security->isGranted('ROLE_ROOT') || $security->isGranted('ROLE_ADMIN')) {
+            if ($lastExhibitSoonSoldOut) {
+                $this->addFlash(
+                    'warning',
+                    'ATTENTION : Le seuil d\'alerte a été atteint pour l\'exposition' . $lastExhibitSoonSoldOut . 'Merci de vérifier l\'état des stocks.'
+                );
+            } elseif ($lastExhibitSoldOut) {
+                $this->addFlash(
+                    'danger',
+                    'URGENT : Le stock est épuisé pour l\'exposition : ' . $lastExhibitSoldOut . '.'
+                );
+            }
+        }
+        
+
         return $this->render('backOffice/index.html.twig', [
         ]);
     }
--- src/Controller/BackOffice/TicketBOController.php
+++ src/Controller/BackOffice/TicketBOController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\BackOffice;
 
 
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\Share\ExhibitionShareRepository;
@@ -12,14 +11,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class TicketBOController extends AbstractController
 {
     #[Route('/backOffice/stockManagement', name: 'stockManagement')]
-    public function ticketStockManagement(
-        ExhibitionShareRepository $exhibitionShareRepo,
-        Security $security): Response
+    public function ticketStockManagement(ExhibitionShareRepository $exhibitionShareRepo): Response
     {
         $exhibitions = $exhibitionShareRepo->findAllNextExhibition();
         $exhibitionStocks = []; // Tableau pour stocker les infos de stock par exposition
 
-        $msgStockAlert = false; // Variable le msg d'alerte de stock 
 
         foreach ($exhibitions as $exhibition) {
             $ticketsReserved = $exhibition->getTicketsReserved();
@@ -34,21 +30,7 @@ final class TicketBOController extends AbstractController
                 'max' => $stockMax,
                 'alert' => $stockAlert,
             ];
-
-        // Vérifie si le stock restant est inférieur ou égal au niveau d'alerte
-        if ($ticketsRemaining <= $stockAlert) {
-            $msgStockAlert = true; // On a au moins une exposition en alerte
-            
-        }
-    }
-
-        // Ajoute un message flash si une alerte de stock est détectée
-        if ($msgStockAlert && ($security->isGranted('ROLE_ROOT') || $security->isGranted('ROLE_ADMIN'))) {
-            $this->addFlash(
-                'warning',
-                'ATTENTION : Merci de vérifier l\'état des stocks, le seuil d\'alerte a été atteint pour au moins une exposition.'
-            );
-        }
+    }            
 
         return $this->render('backOffice/ticket/stockManagement.html.twig', [
             'exhibitionStocks' => $exhibitionStocks,
