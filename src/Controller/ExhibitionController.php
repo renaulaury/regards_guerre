20101b113a46bb1cca2cc0135a257751d0e07cf7
Lily <112404773+Belywen@users.noreply.github.com>
2025-04-28 11:59:03 +0200

    délocalisation des tickets dans page ok

--- src/Controller/ExhibitionController.php
+++ src/Controller/ExhibitionController.php
@@ -4,20 +4,34 @@ namespace App\Controller;
 
 
 use App\Entity\Exhibition;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use App\Service\CartService;
+use App\Repository\TicketRepository;
+use App\Repository\TicketPricingRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ExhibitionController extends AbstractController
 {
     /*Affiche la fiche complète de l'expo*/
     #[Route('/exhibition/{slug}', name: 'exhibition')]
-    public function index(#[MapEntity(mapping: ['slug' => 'slug'])] ?Exhibition $exhibition = null): Response    
+    public function index(
+        #[MapEntity(mapping: ['slug' => 'slug'])] ?Exhibition $exhibition = null,
+        CartService $cartService, 
+        TicketPricingRepository $ticketPricingRepository,): Response 
     {       
+        // Récupérer le panier depuis la session
+        $cart = $cartService->getCart();   
+
+        // Récupérer les prix des tickets pour cette exposition spécifique
+        $ticketPricings = $ticketPricingRepository->findBy(['exhibition' => $exhibition]);
+
         
         return $this->render('/exhibition/index.html.twig', [
+            'ticketPricings' => $ticketPricings,
             'exhibition' => $exhibition,
+            'cart' => $cart, 
         ]);
     }
 }
--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 
-use App\Service\CartService;
-use App\Repository\TicketRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\Share\ExhibitionShareRepository;
@@ -13,25 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class TicketController extends AbstractController
 {
    #[Route('/ticket', name: 'ticket')]
-    public function index(
-        CartService $cartService, 
-        TicketRepository $ticketRepo, 
+    public function index(        
         ExhibitionShareRepository $exhibitShareRepo): Response
     {
-        // Récupérer le panier depuis la session
-        $cart = $cartService->getCart();   
-
-        //Récup de tous les produits par exposition 
-        $priceByExhibit = $ticketRepo->findAllTicketsByExhibition();
-
         //Récup uniquement les infos des prochaines expos
         $exhibitions = $exhibitShareRepo->findAllNextExhibition();
 
 
-        return $this->render('ticket/index.html.twig', [
-            'priceByExhibit' => $priceByExhibit,
-            'exhibitions' => $exhibitions, 
-            'cart' => $cart,          
+        return $this->render('ticket/index.html.twig', [            
+            'exhibitions' => $exhibitions,        
         ]);
     }
 }
