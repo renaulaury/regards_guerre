9df5d5aacf349f3fdc836207757864a5b6f0d8e9
Lily <112404773+Belywen@users.noreply.github.com>
2025-05-04 15:06:12 +0200

    modif envoi facture au user coté admin ok

--- src/Controller/BackOffice/OrderBOController.php
+++ src/Controller/BackOffice/OrderBOController.php
@@ -3,11 +3,11 @@
 namespace App\Controller\BackOffice;
 
 use App\Entity\User;
-use App\Service\OrderService;
 use App\Service\InvoiceService;
 use App\Repository\OrderRepository;
-use App\Service\OrderExportService;
 use App\Service\OrderHistoryService;
+use App\Repository\InvoiceRepository;
+use App\Service\OrderExportPdfService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
@@ -21,7 +21,7 @@ final class OrderBOController extends AbstractController
 /*********** Affiche l'historique de commande de l'utilisateur ************************/
 #[Route('/backOffice/user/userOrderBO/{slug}', name: 'userOrderBO')]        
 public function userOrderBO(    
-    InvoiceService $invoiceService,
+    OrderHistoryService $orderHistoryService,
     #[MapEntity(mapping: ['slug' => 'slug'])] ?User $user = null): Response
 {
     // Vérif de l'accès
@@ -29,7 +29,7 @@ public function userOrderBO(
         return $this->redirectToRoute('home');
     }
 
-    $groupedOrders = $invoiceService->getUserOrderHistory($user->getId());
+    $groupedOrders = $orderHistoryService->getUserOrderHistory($user->getId());
 
     return $this->render('backOffice/user/userOrderBO.html.twig', [
         'groupedOrders' => $groupedOrders,
@@ -39,28 +39,42 @@ public function userOrderBO(
 
 
 
-/***************** Envoi de la réservation en pdf ***********************/
-    #[Route('/backOffice/user/userOrderExportBO/{orderId}', name: 'userOrderExportBO')]
-    public function userOrderExportBO(
-        int $orderId, 
-        OrderExportService $orderExportService,
-        OrderRepository $orderRepo): Response
+/***************** Envoi de la commande en pdf ***********************/
+    #[Route('/backOffice/user/userOrderExportBO/{orderId}', name: 'userInvoiceExportBO')]
+    public function userInvoiceExportBO(
+        int $orderId,
+        OrderRepository $orderRepository,
+        InvoiceRepository $invoiceRepository,
+        InvoiceService $invoiceService): Response
     {
         // Vérif de l'accès
         if (!$this->isGranted('ROLE_ADMIN') && !$this->isGranted('ROLE_ROOT')) {
             return $this->redirectToRoute('home');
         }
 
-        $orderExportService->exportOrder($orderId);
-        
-        $order = $orderRepo->find($orderId); 
+        // Récupère l'entité Order à partir de l'ID.
+        $order = $orderRepository->find($orderId);
 
-    if ($order && $order->getUser()) {
+        // Vérifie si la commande existe et est associée à un utilisateur.
+        if (!$order || !$order->getUser()) {
+            $this->addFlash('error', 'Commande non trouvée.');
+            return $this->redirectToRoute('userOrderBO', ['slug' => $order->getUser()->getSlug()]);
+        }
+
+        // Récupère l'entité Invoice associée à cette commande 
+        $invoice = $invoiceRepository->findOneBy(['numberInvoice' => $order->getNumberInvoice()]);
+
+        // Vérifie si la facture existe.
+        if (!$invoice) {
+            $this->addFlash('error', 'Facture non trouvée pour cette commande.');
+            return $this->redirectToRoute('orderHistory', ['slug' => $order->getUser()->getSlug()]);
+        }
+
+        // Envoie l'email de la facture à l'utilisateur.
+        $invoiceService->sendInvoiceEmail($invoice);
+
+        $this->addFlash('success', 'La facture de votre commande N°' . $invoice->getNumberInvoice() . ' a été envoyée par mail à l\'utilisateur.');
         return $this->redirectToRoute('userOrderBO', ['slug' => $order->getUser()->getSlug()]);
-    } else {
-    
-        return $this->redirectToRoute('backOffice/user/userListBO.html.twig');
-    }
     
     }
 
