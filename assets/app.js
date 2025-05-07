663e1d52fd3a5e4fccf403734642872f78f77bfe
Lily <112404773+Belywen@users.noreply.github.com>
2025-05-07 18:22:05 +0200

    css list expos responsive ok

--- assets/app.js
+++ assets/app.js
@@ -12,7 +12,7 @@ import './styles/navBurgerFooter.css';
 import './styles/home.css';
 import './styles/profile.css';
 import './styles/exhibition.css';
-import './styles/ticketCart.css';
+import './styles/listExhibit_Cart.css';
 
 
 import './styles/backOffice.css';
